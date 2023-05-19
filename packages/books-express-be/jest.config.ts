433b7c1f765dc9934e704002bacfe2cabd92f296
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-05-19 00:04:02 +0000

    first commit

---
+++ packages/books-express-be/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'books-express-be',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/books-express-be',
+};
