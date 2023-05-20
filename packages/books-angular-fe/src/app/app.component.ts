142011fbe22ed0eb3f7e69405559fb37efccb9dc
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-05-20 18:36:43 +0000

    ux ui update

--- packages/books-angular-fe/src/app/app.component.ts
+++ packages/books-angular-fe/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { Apollo, gql } from 'apollo-angular';
 
@@ -9,23 +10,27 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, CommonModule],
+  imports: [NxWelcomeComponent, RouterModule, CommonModule, FormsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   
   posts: any[] = [];
   error: any;
 
-  constructor(private apollo: Apollo) { }
+  isYear = false;
+  isTitle = false;
+  isAuthor = false;
 
-  ngOnInit(): void {
-    this.queryBooksWithGraphQl('title')
-  }
+  year = '';
+  title = '';
+  author = '';
+
+  constructor(private apollo: Apollo) { }
   
-  queryBooksWithGraphQl(year?: string, title?: string, author?: string) {
+  queryBooksWithGraphQl(year = this.year, title = this.title, author = this.author) {
     this.apollo.watchQuery({
       query: gql`
       query ExampleQuery {
@@ -42,6 +47,18 @@ export class AppComponent implements OnInit {
       console.log("POSTS -> ", data)
     }
     );
+  }
+
+  submit() {
+    this.updateValuesFromCheckbox();
+    this.queryBooksWithGraphQl(this.year, this.author, this.title)
+  }
+
+  updateValuesFromCheckbox() {
+    this.isYear === true ? this.year = 'year' : this.year = '';
+    this.isAuthor === true ? this.author = 'author' : this.author = '';
+    this.isTitle === true ? this.title = 'title' : this.title = '';
+
   }
 
 }
